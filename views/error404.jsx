d74a67293b24882b079c5d0936855c8fdbf3d34f
natalie <nataliemae263@gmail.com>
2022-03-13 14:39:46 -0500

    finished images 404 page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,10 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <div>
+                  <img src="./images/2021EasterPurpleChick.jpg" width={500} height={600} alt="St Mary's Hospital Milw, WI. Historical Tower" />
+              </div>
+              Photo By Natalie Leonard
           </main>
       </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -7,8 +7,9 @@ function home () {
             <main>
                 <h1>HOME</h1>
                 <div>
-                  <img src="/public/images/redvelvetcupcake.jpg" alt="Chia Fruit Shake" />
+                  <img src="./images/redvelvetcupcake.jpg" width={450} height={500} alt="Red Velvet Cupcake" />
                 </div>
+                Photo by <a href="https://unsplash.com/@luisanazl?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">luisana zerpa</a> on <a href="https://unsplash.com/s/photos/desserts?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
                 <a href="/places">
                     <button className="btn-primary">Places Page</button>
                 </a>
