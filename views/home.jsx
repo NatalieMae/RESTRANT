f0037379d005434e7e443f0d1cb74fcc5824ca14
natalie <nataliemae263@gmail.com>
2022-03-13 13:55:28 -0500

    picture problems

--- views/home.jsx
+++ views/home.jsx
@@ -3,15 +3,18 @@ const Def = require('./default')
 
 function home () {
     return (
-      <Def>
-          <main>
-            <h1>HOME</h1>
-              <a href="/places">
-                <button className="btn-primary">Places Page</button>
-              </a>
-          </main>
-      </Def>
+        <Def>
+            <main>
+                <h1>HOME</h1>
+                <div>
+                  <img src="/public/images/redvelvetcupcake.jpg" alt="Chia Fruit Shake" />
+                </div>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
+            </main>
+        </Def>
     )
-  }
+}
 
 module.exports = home
