91e03be7c4103ddd4317d47073b3b9e96f9b94d4
natalie <nataliemae263@gmail.com>
2022-03-14 19:52:28 -0500

    Finally End of part 5 HAHA ;)

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: './images/ThaiCrabs.jpg'
+      }, {
+        name: 'Coding Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: './images/BasketCookieswcoffee.jpg'
+      }]
