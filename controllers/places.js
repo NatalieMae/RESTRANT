f7906f751024a6f3fd76f64afc8acbed376899c1
natalie <nataliemae263@gmail.com>
2022-03-14 21:33:13 -0500

    fixed it

--- controllers/places.js
+++ controllers/places.js
@@ -25,7 +25,6 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
   console.log(req.body)
   if (!req.body.pic) {
-    // Default image if one is not provided
     req.body.pic = 'http://placekitten.com/400/400'
   }
   if (!req.body.city) {
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+//Methods and Globals
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
-
+//Express Settings
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
