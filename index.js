dd02e7e6f0be8d3e80e119009d798c4243de1961
natalie <nataliemae263@gmail.com>
2022-03-19 13:54:46 -0500

    Delete Button done working on the edit button

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-//Methods and Globals
+//Dependencies and Configuration
+// Modules and Globals
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const methodOverride = require('method-override')
 
-//Express Settings
+//Middleware
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.use('/places', require('./controllers/places'))
 
+//Routes
 app.get('/', (req, res) => {
   res.render('home')
 })
 
+//404 Page
 app.get('*', (req, res) => {
     res.render('error404')
 })
 
+//Listener
 app.listen(process.env.PORT)
