4bd0ce4857bd69912886d9ad5a341a72cda12dd0
natalie <nataliemae263@gmail.com>
2022-03-22 18:39:54 -0500

    end of starting a seeder file, not working correctly require not a function and uri is undefined

--- models/index.js
+++ models/index.js
@@ -1,8 +1,12 @@
+require('dotenv').require()
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true, 
-    useUnifiedTopology: true
-}, () => { console.log('connected to mongo: ', process.env.MONGO_URI) } )
+    useUnifiedTopology: true,
+            },
+        () => { console.log('connected to mongo: ', process.env.MONGO_URI) 
+    } 
+)
 
 module.exports.Place = require('./places')
---
+++ seeders/seed-places.js
@@ -0,0 +1,26 @@
+
+const db = require('../models')
+
+db.Place.create([{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/public/images/ThaiBowl.jpg',
+    founded: 1989
+}, {
+    name: 'Coding Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/public/images/BakeryCase.jpg',
+    founded: 2020
+}])
+.then(() => {
+    console.log('Success!')
+    process.exit()
+})
+.catch(err => {
+    console.log('Failure!', err)
+    process.exit()
+})
