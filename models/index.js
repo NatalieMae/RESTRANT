505d07d25a1e9a79f6eb8c3e7245590a84e61a32
unknown <nataliemae263@gmail.com>
2022-03-23 19:55:19 -0500

    End of part 8

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-require('dotenv').require()
+require('dotenv').config()     //Houston We have a problem here Per Terminal
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI, {
